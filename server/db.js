28144b6a518327b67c4e044242b727e3dd7165ef
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-03-31 19:15:50 +0500

    database change

--- server/db.js
+++ server/db.js
@@ -1,23 +1,23 @@
-// const { Pool } = require('pg')
-// const pool = new Pool({
-//     user: "postgres",
-//     password: "VCaToZvZwFMRmnpVBZyIAyRRBOTZcGJi",
-//     host: "tramway.proxy.rlwy.net",
-//     port: 13154,
-//     database: "railway"
-// })
-
-// module.exports = pool
-
-
-
 const { Pool } = require('pg')
 const pool = new Pool({
     user: "postgres",
-    password: "stalin",
-    host: "localhost",
-    post: 5432,
-    database: "igropoisk"
+    password: "VCaToZvZwFMRmnpVBZyIAyRRBOTZcGJi",
+    host: "tramway.proxy.rlwy.net",
+    port: 13154,
+    database: "railway"
 })
 
 module.exports = pool
+
+
+
+// const { Pool } = require('pg')
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "stalin",
+//     host: "localhost",
+//     post: 5432,
+//     database: "igropoisk"
+// })
+
+// module.exports = pool
