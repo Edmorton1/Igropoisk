9b3cd10c42645ebf99bc69ddf2c2e022e797981b
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-04-02 19:07:14 +0500

    cors

--- server/index.js
+++ server/index.js
@@ -1,16 +1,43 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-
+const cors =require('cors')
+const cookieParser = require('cookie-parser')
+const router = require('./router')
 const app = express()
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const db = require('./db.js')
+const path = require('path')
 
+// app.use(bodyParser.json({limit: "50mb"}))
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+// app.use(compression())
+// app.use(express.json())
 app.use(cors())
+// app.use(cookieParser())
 
-app.get('/data', (req, res) => {
-  res.json('dathjghghjgjha')
-})
+// const user = async (req, res, next) => {
+//     try {
+//         const accessToken = req.header("Authorization").split(' ')[1].split('.')[1]
+//         const id = JSON.parse(atob(accessToken)).id
+//         const total = (await db.query(`SELECT * FROM users WHERE id = $1`, [id])).rows[0]
+//         req.user = id
+//         console.log(`${id}, МИДЛВЕЙР`)
+//     } catch {
+        
+//     } finally {
+//         next()
+//     }
+// }
+
+// app.use('/avatars', express.static(path.resolve(__dirname, 'avatars')));
+// app.use(user)
+
+// app.use('/api', router)
 
 app.get('/', (req, res) => {
-  res.json('dathjghghjgjha')
+    res.json('asdasdasdasd')
 })
 
-app.listen(3000, () => console.log('SERVER'))
+app.listen(3000, () => {console.log(`СЕРВЕР ЗАПУЩЕН НА ПОРТУ: ${process.env.BACKEND_SERVER}`)})
