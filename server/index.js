0d5ec1f16afb0acf8aad4a034fb1924fa325aabe
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-04-02 18:56:03 +0500

    Урезанный сервер

--- server/index.js
+++ server/index.js
@@ -9,34 +9,38 @@ const bodyParser = require('body-parser')
 const db = require('./db.js')
 const path = require('path')
 
-app.use(bodyParser.json({limit: "50mb"}))
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.json({limit: "50mb"}))
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(compression())
-app.use(express.json())
+// app.use(compression())
+// app.use(express.json())
 app.use(cors({
     origin: process.env.FRONTEND_CLIENT,
     credentials: true
 }))
-app.use(cookieParser())
+// app.use(cookieParser())
 
-const user = async (req, res, next) => {
-    try {
-        const accessToken = req.header("Authorization").split(' ')[1].split('.')[1]
-        const id = JSON.parse(atob(accessToken)).id
-        const total = (await db.query(`SELECT * FROM users WHERE id = $1`, [id])).rows[0]
-        req.user = id
-        console.log(`${id}, МИДЛВЕЙР`)
-    } catch {
+// const user = async (req, res, next) => {
+//     try {
+//         const accessToken = req.header("Authorization").split(' ')[1].split('.')[1]
+//         const id = JSON.parse(atob(accessToken)).id
+//         const total = (await db.query(`SELECT * FROM users WHERE id = $1`, [id])).rows[0]
+//         req.user = id
+//         console.log(`${id}, МИДЛВЕЙР`)
+//     } catch {
         
-    } finally {
-        next()
-    }
-}
+//     } finally {
+//         next()
+//     }
+// }
 
-app.use('/avatars', express.static(path.resolve(__dirname, 'avatars')));
-app.use(user)
+// app.use('/avatars', express.static(path.resolve(__dirname, 'avatars')));
+// app.use(user)
 
-app.use('/api', router)
+// app.use('/api', router)
 
-app.listen(process.env.PORT, () => {console.log(`СЕРВЕР ЗАПУЩЕН НА ПОРТУ: ${process.env.BACKEND_SERVER}`)})
+app.get('/', (req, res) => {
+    res.json('asdasdasdasd')
+})
+
+app.listen(process.env.PORT || 3000, () => {console.log(`СЕРВЕР ЗАПУЩЕН НА ПОРТУ: ${process.env.BACKEND_SERVER}`)})
